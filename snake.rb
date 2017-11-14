b70ee0c9cfc3af29edacd26e94cd9f4cbaa058ce
Mateusz <lesniak.mat@gmail.com>
2017-11-14 21:12:00 +0100

    .

--- snake.rb
+++ snake.rb
@@ -17,7 +17,7 @@ COLS = 80
 class Snake
   attr_reader :location
   def initialize
-    @segments = 2
+    @segments = 20
     @location = []
   end
 
@@ -39,37 +39,36 @@ def set_fruit(location, window)
   next if location.include?([y, x])
   current = [window.cury, window.curx]
   window.setpos(y, x)
+  window.color_set(3)
   window.addch('@')
+  window.color_set(2)
   window.setpos(current[0], current[1])
   break
   end
 end
 
-
-
-
 Curses.init_screen      #nie mam pojęcia po co to wszyscy wrzucają
 Curses.noecho
 Curses.start_color
-Curses.init_pair(1, Curses::COLOR_RED, Curses::COLOR_BLACK)
-Curses.init_pair(2, Curses::COLOR_BLUE, Curses::COLOR_GREEN)
+Curses.init_pair(1, Curses::COLOR_RED, Curses::COLOR_RED)
+Curses.init_pair(2, Curses::COLOR_GREEN, Curses::COLOR_GREEN)
+Curses.init_pair(3, Curses::COLOR_GREEN, Curses::COLOR_BLACK)
 
 window = Curses::Window.new(ROWS, COLS, 0, 0)
-window.color_set(1)
 # window.scrollok(true)
 # window.idlok(true)     #sprawdzić później co robi idlok bo nie łapie do końca
 window.keypad = true
 window.nodelay = true # temu leci i nie czeka
 
-
-window.addstr('Testing')
-window.setpos(20, 40)
-window.box('|', '-', '+')
+window.color_set(1)
+window.box('.', '.', '.')
 window.color_set(2)
+window.setpos(20, 40)
 
 set_fruit([], window)
 snake = Snake.new
 input = Curses::KEY_UP
+
 until (input = window.getch || input) == 'q'
   # window.addstr("current x is #{window.curx}")
   # window.addstr("current begy is #{window.begy}")
@@ -82,8 +81,8 @@ until (input = window.getch || input) == 'q'
     set_fruit(snake.location, window)
   end
 
-  if ['.', '|', '-'].include?(window.inch.chr)
-    window.color_set(1)
+  if window.inch.chr == '.'
+    window.color_set(3)
     # puts "#{window.curx}, #{window.cury}"     # a to w ogóle wypluwał na poprzedzającym zderzenie polu, ale zakładam że to kwestia wpychania putsa do curses
     window.addch('x')      # tego nie chce za cholere zrobic
     sleep(3)      # to realizuje
@@ -93,7 +92,7 @@ until (input = window.getch || input) == 'q'
   window.addch('.')
   head = [window.cury, window.curx - 1]
   if tail
-    window.color_set(1)
+    window.color_set(3)
     window.setpos(tail[0], tail[1])
     window.addch(' ')
     window.color_set(2)
